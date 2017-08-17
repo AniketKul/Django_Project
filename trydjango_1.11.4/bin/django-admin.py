d801d3bfcc3a7405f0b6547f2282e620f57465b8
Aniket Kulkarni <aniketmilindkulkarni@gmail.com>
2017-08-16 21:11:38 -0700

    Initial Commit

---
+++ trydjango_1.11.4/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Volumes/DATA/Coursera_My_Courses/Django_Project/trydjango_1.11.4/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
